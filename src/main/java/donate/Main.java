0b10279ab316cd0d13ad1894835cbc01aa4a2aca
Srivathsav S <sreevathsav2002@gmail.com>
2025-10-04 16:26:54 +0530

    Update
    Update

--- src/main/java/donate/Main.java
+++ src/main/java/donate/Main.java
@@ -1,11 +1,15 @@
 package donate;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
+        Dotenv dotenv = Dotenv.load(); // Loads .env from the current directory
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
         // TODO: Create an adapter to switch between dbs easily
         SpringApplication.run(Main.class,args);
     }
